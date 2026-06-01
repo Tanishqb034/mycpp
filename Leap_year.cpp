473e179f434fe418a2f8488dddb50e329be175e6
Tanishq <tanishqb034@gmail.com>
2026-06-01 14:21:27 +0530

    Leap_year_code

---
+++ Leap_year.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int leap(int n)
+{
+    if(n%400==0)
+    {
+        return 1;
+    }
+    else if (n%4==0 && n%100!=0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+}
+int main() {
+ int n,y;
+ cin>>n;
+ y=leap(n);
+ cout<<y;
+    return 0;
+}
