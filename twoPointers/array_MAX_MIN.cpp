f939a05b46e6449ddd44a3212961076b9a71ee84
Tanishq <tanishqb034@gmail.com>
2026-06-06 19:25:27 +0530

    THIS IS MAX MIN ARRAY

---
+++ twoPointers/array_MAX_MIN.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    
+    for(int i=0; i<5; i++) {
+        cin >> arr[i];
+    }
+
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i=1; i<5; i++) {
+        if(arr[i] > max) {
+            max = arr[i];
+        }
+
+        if(arr[i] < min) {
+            min = arr[i];
+        }
+    }
+
+    cout << "Maximum = " << max << endl;
+    cout << "Minimum = " << min << endl;
+
+    return 0;
+}
