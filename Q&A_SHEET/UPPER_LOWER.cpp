6785b55be2a817bc3735760cf88625425f562485
Tanishq <tanishqb034@gmail.com>
2026-06-17 14:16:41 +0530

    UPPER TO LOWER

---
+++ Q&A_SHEET/UPPER_LOWER.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+char Convert(char name)
+{
+    char ans=name+'a'-'A'; 
+    return ans;
+}
+int main() {
+char name;
+cin>>name;
+cout<<Convert(name);
+    return 0;
+}
